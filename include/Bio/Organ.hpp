83de5cab6afa78113e48210a5a0a969ef9f490ac
Mustafa <mmermmus123@gmail.com>
2022-12-17 16:20:50 +0300

    Organ class filled up, there are lots of todos.

--- include/Bio/Organ.hpp
+++ include/Bio/Organ.hpp
@@ -12,10 +12,40 @@ to gain that structure tissue ought to have node properities.
 class Organ
 {
 private:
-    // Tissue *tissues = new Tissue[20]; //@ that line will come as parameter.
+    // status: organ can accept tissue data type from out.
+    // adds datas to tissue tree according to tissue's median value.
+
+    // TODO: max tissue limit will be codded. like max 20 organ can accept.
+    // Callout: may controll class send tissues as block of 20s. ~(or)~
+    // we collect tissues together in an seperate array as 20s then apply adding operation each of them ??
+    // TODO: tree balance controll will be coded.
+    // TODO: delete unneccesary methods.
+    // TODO: store tree balance status for system class for future.
+private:
+    Tissue *root;
+    void SearchAndAdd(Tissue *&subNode, const Tissue &newItem);
+    bool SearchAndDelete(Tissue *&subNode, const Tissue &data);
+    bool DeleteNode(Tissue *&subNode);
+    void inorder(Tissue *subNode);
+    void preorder(Tissue *subNode);
+    void postorder(Tissue *subNode);
+
+    int Height(Tissue *subNode);
+    void PrintLevel(Tissue *subNode, int level);
+    bool Search(Tissue *subNode, const Tissue &item);
 
 public:
     Organ();
+    bool isEmpty() const;
+    void Add(const Tissue &newItem);
+    void Delete(const Tissue &data);
+    void inorder();
+    void preorder();
+    void postorder();
+    void levelorder();
+    int Height();
+    bool Search(const Tissue &item);
+    void Clear();
     ~Organ();
 };
 
--- src/Bio/Organ.cpp
+++ src/Bio/Organ.cpp
@@ -1,10 +1,165 @@
 #include "../../include/Bio/Organ.hpp"
 
-Organ::Organ()
+// PRIVATE METHODS
+void Organ::SearchAndAdd(Tissue *&subNode, const Tissue &newItem)
 {
+    if (subNode == NULL)
+        subNode = new Tissue(newItem);
+    else if (newItem.median < subNode->calculateMedian())
+        SearchAndAdd(subNode->left, newItem);
+    else if (newItem.median > subNode->median)
+        SearchAndAdd(subNode->right, newItem);
+    else
+        SearchAndAdd(subNode->left, newItem);
+}
+bool Organ::SearchAndDelete(Tissue *&subNode, const Tissue &data)
+{
+    if (subNode == NULL)
+        return false;
+    if (subNode->median == data.median)
+        return DeleteNode(subNode);
+    else if (data.median < subNode->median)
+        return SearchAndDelete(subNode->left, data);
+    else
+        return SearchAndDelete(subNode->right, data);
+}
+bool Organ::DeleteNode(Tissue *&subNode)
+{
+    Tissue *DelNode = subNode;
+
+    if (subNode->right == NULL)
+        subNode = subNode->left;
+    else if (subNode->left == NULL)
+        subNode = subNode->right;
+    else
+    {
+        DelNode = subNode->left;
+        Tissue *parent = subNode;
+        while (DelNode->right != NULL)
+        {
+            parent = DelNode;
+            DelNode = DelNode->right;
+        }
+        subNode->median = DelNode->median;
+        if (parent == subNode)
+            subNode->left = DelNode->left;
+        else
+            parent->right = DelNode->left;
+    }
+    delete DelNode;
+    return true;
+}
+void Organ::inorder(Tissue *subNode)
+{
+    if (subNode != NULL)
+    {
+
+        inorder(subNode->left);
+        cout << subNode->calculateMedian() << " ";
+        inorder(subNode->right);
+    }
+}
+void Organ::preorder(Tissue *subNode)
+{
+    if (subNode != NULL)
+    {
+        cout << subNode->calculateMedian() << " ";
+        preorder(subNode->left);
+        preorder(subNode->right);
+    }
+}
+void Organ::postorder(Tissue *subNode)
+{
+    if (subNode != NULL)
+    {
+        postorder(subNode->left);
+        postorder(subNode->right);
+        cout << subNode->calculateMedian() << " ";
+    }
 }
 
+int Organ::Height(Tissue *subNode)
+{
+    if (subNode == NULL)
+        return -1;
+    return 1 + max(Height(subNode->left), Height(subNode->right));
+}
+void Organ::PrintLevel(Tissue *subNode, int level)
+{
+    if (subNode == NULL)
+        return;
+    if (level == 0)
+        cout << subNode->calculateMedian() << " ";
+    else
+    {
+        PrintLevel(subNode->left, level - 1);
+        PrintLevel(subNode->right, level - 1);
+    }
+}
+bool Organ::Search(Tissue *subNode, const Tissue &item)
+{
+    if (subNode == NULL)
+        return false;
+    if (subNode->calculateMedian() == item.median)
+        return true;
+    if (item.median < subNode->calculateMedian())
+        return Search(subNode->left, item);
+    else
+        return Search(subNode->right, item);
+}
+
+// PUBLIC METHODS
+Organ::Organ()
+{
+    root = NULL;
+}
+bool Organ::isEmpty() const
+{
+    return root == NULL;
+}
+void Organ::Add(const Tissue &newItem)
+{
+    SearchAndAdd(root, newItem);
+}
+void Organ::Delete(const Tissue &data)
+{
+    if (SearchAndDelete(root, data) == false)
+        throw "Item not found.";
+}
+void Organ::inorder()
+{
+    inorder(root);
+}
+void Organ::preorder()
+{
+    preorder(root);
+}
+void Organ::postorder()
+{
+    postorder(root);
+}
+void Organ::levelorder()
+{
+    int h = Height();
+    for (int level = 0; level <= h; level++)
+    {
+        PrintLevel(root, level);
+    }
+}
+int Organ::Height()
+{
+    return Height(root);
+}
+bool Organ::Search(const Tissue &item)
+{
+    return Search(root, item);
+}
+void Organ::Clear()
+{
+    while (!isEmpty())
+        DeleteNode(root);
+}
 Organ::~Organ()
 {
-    // TODO:
+    Clear();
 }
