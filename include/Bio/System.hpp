ee126daa1595f49344ce35e8215f430371a421f0
Mustafa <mmermmus123@gmail.com>
2022-12-20 22:22:37 +0300

    system updated

--- include/Bio/System.hpp
+++ include/Bio/System.hpp
@@ -2,26 +2,14 @@
 #define SYSTEM
 
 #include "Organ.hpp"
+using namespace std;
 class System
 {
-private:
-    Organ *head;
-    Organ *FindPrevByPosition(int position);
-
 public:
+    System *next;
+    Organ **organs;
     System();
-    bool isEmpty() const;
-    int size() const;
-    Organ first();
-    Organ last();
-    void add(const Organ &item);
-    void insert(int index, const Organ &item);
-    void remove(const Organ &item);
-    void removeAt(int index);
-    bool find(const Organ &item) const;
-    int indexOf(const Organ &item);
-    friend ostream &operator<<(ostream &screen, System &right);
-    void clear();
+    System(Organ **, System *);
     ~System();
 };
 
