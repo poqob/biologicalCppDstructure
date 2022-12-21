b32b3ad83e5216cf531420ed1f6675a66deca4ec
Mustafa <mmermmus123@gmail.com>
2022-12-21 04:37:41 +0300

    code tidied up

--- src/program.cpp
+++ src/program.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
 #include "../include/util/Timer.hpp"
 #include "../include/util/Debug.hpp"
-#include "../include/Bio/Cell.hpp"
-#include "../include/Bio/Tissue.hpp"
-#include "../include/radixsort/Queue2.hpp"
-#include "../include/Bio/Organ.hpp"
-#include "../include/radixsort/Radix2.hpp"
 #include "../include/fileOperations/ReadFile.hpp"
 #include "../include/UI/UI.hpp"
 
@@ -14,21 +9,20 @@ using namespace std;
 int main()
 {
     Debug::change_debug_status(0); // 1 to debug mode, 0 to non debug mode.
-    Timer timer;
-    char s;
-    Ui ui = Ui();
+    Timer timer;                   // debug mode feature. I measured time for every single debug, it's only curious.
+    Ui ui = Ui();                  // UI, it's only related with visual output thats the reason i didn't new it on heap.
     timer.start();
 
-    Controll *controller = new Controll();
-    ReadFile *rf = new ReadFile(controller);
-    ui.draw("ORGANIZMA", controller->showCreature());
-    controller->organism->mutation();
-    ui.draw("ORGANIZMA (MUTASYONA UGRADI)", controller->showCreature());
+    Controll *controller = new Controll();                               // controll class
+    ReadFile *rf = new ReadFile(controller);                             // read file class
+    ui.draw("ORGANIZMA", controller->showCreature());                    // drawing output to console with specified header
+    controller->organism->mutation();                                    // mutate the organism
+    ui.draw("ORGANIZMA (MUTASYONA UGRADI)", controller->showCreature()); // redraw the organism
 
     timer.stop();
-    Debug::debugPrinter("\ntime elapsed: ", timer.elapsedSeconds());
+    Debug::debugPrinter("\ntime elapsed: ", timer.elapsedSeconds()); // debeug mode printer
 
     delete rf;
-    delete controller;
+    delete controller; // sub deletions in controller destructor
     return 0;
 }
